04e9254396a9dbde3231f2171a0af67508b3e184
matthew-Bins <matthew.f.bins@gmail.com>
2016-04-14 14:13:41 -0600

    did some more functions

--- Game.cpp
+++ Game.cpp
@@ -15,7 +15,54 @@ namespace Gaming {
 	const double Game::STARTING_RESOURCE_CAPACITY = 10;
 	PositionRandomizer Game::__posRandomizer = PositionRandomizer();
 
-	void Game::populate() {///////////////////////////////
+	void Game::populate() {
+
+		__numInitAgents = (__width * __height) / NUM_INIT_AGENT_FACTOR;
+		__numInitResources = (__width * __height) / NUM_INIT_RESOURCE_FACTOR;
+		unsigned int numStrategic = __numInitAgents / 2;
+		unsigned int numSimple = __numInitAgents - numStrategic;
+		unsigned int numAdvantages = __numInitResources / 4;
+		unsigned int numFoods = __numInitResources - numAdvantages;
+		//random number generator
+		std::default_random_engine gen;
+		std::uniform_int_distribution<int> d(0, __width * __height);
+
+		while (numStrategic > 0) {
+			int i = d(gen); // random index in the grid vector
+			if (__grid[i] == nullptr) { // is position empty
+				Position pos(i / __width, i % __width);
+				__grid[i] = new Strategic(*this, pos, Game::STARTING_AGENT_ENERGY);
+				numStrategic--;
+			}
+		}
+
+		while (numSimple > 0) {
+			int i = d(gen); // random index in the grid vector
+			if (__grid[i] == nullptr) { // is position empty
+				Position pos(i / __width, i % __width);
+				__grid[i] = new Simple(*this, pos, Game::STARTING_AGENT_ENERGY);
+				numSimple--;
+			}
+		}
+
+		while (numAdvantages > 0) {
+			int i = d(gen); // random index in the grid vector
+			if (__grid[i] == nullptr) { // is position empty
+				Position pos(i / __width, i % __width);
+				__grid[i] = new Advantage(*this, pos, Game::STARTING_RESOURCE_CAPACITY);
+				numAdvantages--;
+			}
+		}
+
+		while (numFoods > 0) {
+			int i = d(gen); // random index in the grid vector
+			if (__grid[i] == nullptr) { // is position empty
+				Position pos(i / __width, i % __width);
+				__grid[i] = new Food(*this, pos, Game::STARTING_RESOURCE_CAPACITY);
+				numFoods--;
+			}
+		}
+
 
 	}
 
@@ -41,8 +88,10 @@ namespace Gaming {
 			populate();
 	}
 
-	Game::~Game() {///////////////////////////////////////
-
+	Game::~Game() {
+		for (auto it = __grid.begin(); it != __grid.end(); ++it)
+			if (*it != nullptr)
+				delete *it;
 	}
 
 	unsigned int Game::getNumPieces() const { 
@@ -188,7 +237,7 @@ namespace Gaming {
 		__grid[place] = new Advantage(*this, p, STARTING_RESOURCE_CAPACITY);
 	}
 
-	const Surroundings Game::getSurroundings(const Position& pos) const {/////////////////
+	const Surroundings Game::getSurroundings(const Position& pos) const {
 		Surroundings s; //makes an array
 
 		for (int i = 0; i < 9; ++i) { //fill array
@@ -225,16 +274,72 @@ namespace Gaming {
 		return s;
 	}
 
-	const ActionType Game::reachSurroundings(const Position& from, const Position& to) {///////////////
+	const ActionType Game::reachSurroundings(const Position& from, const Position& to) {
+		if (from.x == to.x && from.y == from.x)
+			return STAY;
 		
-		return E;
+		if (to.x == 0 && to.y == 0)
+			return NW;
+		else if (to.x == 1 && to.y == 0)
+			return N;
+		else if (to.x == 2 && to.y == 0)
+			return NE;
+		else if (to.x == 0 && to.y == 1)
+			return W;
+		else if (to.x == 2 && to.y == 1)
+			return E;
+		else if (to.x == 0 && to.y == 2)
+			return SW;
+		else if (to.x == 1 && to.y == 2)
+			return S;
+		else if (to.x == 2 && to.y == 2)
+			return SE;
 	}
 
-	bool Game::isLegal(const ActionType& ac, const Position& pos) const {//////////////////////
-		return true;
+	bool Game::isLegal(const ActionType& ac, const Position& pos) const {
+		Surroundings s;
+		s = getSurroundings(pos);
+		bool legal = true;
+
+		switch (ac) {
+		case N:
+			if (s.array[1] != INACCESSIBLE) legal = true;
+			break;
+		case NE:
+			if (s.array[2] != INACCESSIBLE) legal = true;
+			break;
+		case NW:
+			if (s.array[0] != INACCESSIBLE) legal = true;
+			break;
+		case E:
+			if (s.array[5] != INACCESSIBLE) legal = true;
+			break;
+		case W:
+			if (s.array[3] != INACCESSIBLE) legal = true;
+			break;
+		case SE:
+			if (s.array[8] != INACCESSIBLE) legal = true;
+			break;
+		case SW:
+			if (s.array[6] != INACCESSIBLE) legal = true;
+			break;
+		case S:
+			if (s.array[7] != INACCESSIBLE) legal = true;
+			break;
+		case STAY:
+			legal = true;
+			break;
+		}
+		return legal;
 	}
 
-	const Position Game::move(const Position& pos, const ActionType& ac) const {////////////////////
+	const Position Game::move(const Position& pos, const ActionType& ac) const {
+
+		do {
+
+
+		} while (!isLegal(ac, pos));
+
 		return pos;
 	}
 
